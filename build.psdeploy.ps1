6b1f2860712f5cd6ce4b3fdb97464e6b87367c7e
cofl <cllacour@mtu.edu>
2019-02-13 19:13:53 -0500

    Updated build script.

--- build.psdeploy.ps1
+++ build.psdeploy.ps1
@@ -1,8 +1,15 @@
 #Requires -module PSDeploy
 
-# [string]$ModuleName = Get-Item src/*.psd1 | Where-Object {Test-ModuleManifest -Path $_ -ErrorAction SilentlyContinue} | Select-Object -First 1 -ExpandProperty BaseName
+[string]$ModuleName = Get-Item "$PSScriptRoot/src/*.psd1" | Where-Object {Test-ModuleManifest -Path $_ -ErrorAction SilentlyContinue} | Select-Object -First 1 -ExpandProperty BaseName
 
 Deploy Public {
     # see docs for PSDeploy to put your deployment here.
     # but seriously consider not doing that and just using the official version from PSGallery instead.
+    By PSGalleryModule {
+        FromSource "$PSScriptRoot/Release/$ModuleName"
+        To PSGallery
+        WithOptions @{
+            ApiKey = $env:PSGalleryKeyCoflOSDManagement
+        }
+    }
 }
